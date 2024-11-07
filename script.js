2e067b193b58744f687083fd8e1b0917c798392a
marawan350 <wana2432006@gmail.com>
2024-11-07 16:31:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -58,47 +58,4 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     // تحميل FingerprintJS
-    const fp = await FingerprintJS.load();
-    const result = await fp.get();
-
-    // جلب بصمة الجهاز الحالية
-    const currentFingerprint = result.visitorId;
-    console.log('Current Fingerprint:', currentFingerprint);
-
-    // قائمة البصمات المسموح بها
-    const allowedFingerprints = [
-        'dd740041a38520f1892433695e84845c',
-        '09ebc4777f2d8d915fb8b093d1cc7d30',
-        'a6059e344eeeb44f0b5054277fab0518',
-        'a4451d1b96fc0779fade24230883e46a',
-        // أضف المزيد من البصمات المسموح بها هنا
-    ];
-
-    // التحقق من إذا كانت البصمة الحالية مسموح بها
-    if (!allowedFingerprints.includes(currentFingerprint)) {
-        document.body.innerHTML = `
-            <div class="container" style="text-align: center; margin-top: 100px;">
-                <h2>طلب إذن الوصول</h2>
-                <a href="https://wa.me/+201154180620" class="request-button" style="
-                    display: inline-block;
-                    background-color: #4CAF50;
-                    color: white;
-                    padding: 15px 30px;
-                    font-size: 18px;
-                    text-decoration: none;
-                    border-radius: 5px;
-                    transition: background-color 0.3s;
-                ">طلب إذن</a>
-                <p style="font-size: 20px; margin-top: 20px; color: #333;">
-                  يُرجى النقر على الزر بالأعلى لطلب الإذن.
-                </p>
-                <p style="font-size: 18px; margin-top: 10px; color: #555;">
-                  معرف جهازك الحالي: ${currentFingerprint}
-                </p>
-            </div>
-        `;
-        alert(`جهازك ليس لديه إذن وصول.`);
-    } else {
-        console.log('Access granted');
-    }
 });
